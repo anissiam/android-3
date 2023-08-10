181faa44526c456d5908003203b1ca31fbab8be8
anissiam <anis.siam@hotmail.com>
2023-08-10 14:52:33 +0300

    -constraintlayout -FramLayout

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_ex2);
+        setContentView(R.layout.constraintlayout_ex2);
 
 
     }
